d7d7cbfd2e3cc0540cebd5c430eb7b776befa9df
rebaiChedly <107418294+ChedlyRebai@users.noreply.github.com>
2026-02-13 11:37:29 +0100

    Implement CandidatService with CRUD operations and update candidatREstAPi to use the service

--- src/main/java/com/candidate/chedly/candidatREstAPi.java
+++ src/main/java/com/candidate/chedly/candidatREstAPi.java
@@ -2,23 +2,46 @@ package com.candidate.chedly;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 
 @RestController
 @RequestMapping("/candidats")
 public class candidatREstAPi {
     
+    @Autowired
+    private CandidatService candidatService;
     private String title="hello world";
 
     @GetMapping
-    public String getHello(@RequestParam String param) {
-        return this.title;
+    public ResponseEntity<List<Candidat>> getCandidats() {
+        return ResponseEntity.ok(candidatService.getAllCandidats());
     }
-    
-    @GetMapping("/database")
-    public String getDatabaseName() {
-        return "";
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Candidat> updateCandidat(@PathVariable Long id,@RequestBody Candidat body){
+        return  ResponseEntity.ok(this.candidatService.updateCandidat(id, body));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> DeleteCandidat(@PathVariable Long id){
+        return ResponseEntity.ok(this.candidatService.deleteCandidat(id));
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Candidat> addCandida(@RequestBody Candidat candidat) {
+        return ResponseEntity.ok(this.candidatService.addCandidat(candidat));
     }
 }
